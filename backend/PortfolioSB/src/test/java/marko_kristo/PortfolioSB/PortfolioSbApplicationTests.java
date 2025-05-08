6c1135a44bfb315e476f2de3746e2f4bf13793df
Marke1996 <133226968+Marke1996@users.noreply.github.com>
2025-05-08 17:56:33 +0200

    SpringBoot und Angular App Erstellt

---
+++ backend/PortfolioSB/src/test/java/marko_kristo/PortfolioSB/PortfolioSbApplicationTests.java
@@ -0,0 +1,13 @@
+package marko_kristo.PortfolioSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
