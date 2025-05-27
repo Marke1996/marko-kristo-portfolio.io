5de064d790e547dab089782cd3983df0bab499d2
Marke1996 <133226968+Marke1996@users.noreply.github.com>
2025-05-27 17:13:44 +0200

    home-page component created

--- frontend/PortfolioA/src/app/app.routes.ts
+++ frontend/PortfolioA/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './component/home-page/home-page.component';
 
 export const routes: Routes = [
-
-  ];
+   {
+        path: '',
+        component: HomePageComponent,
+     }
+];
