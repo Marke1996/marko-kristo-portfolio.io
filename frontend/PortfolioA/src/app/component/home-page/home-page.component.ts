09f98550f0121247e7fc73734b9cde85e03e038d
Marke1996 <133226968+Marke1996@users.noreply.github.com>
2025-06-13 13:48:48 +0200

    projekt section

--- frontend/PortfolioA/src/app/component/home-page/home-page.component.ts
+++ frontend/PortfolioA/src/app/component/home-page/home-page.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home-page',
   imports: [],
@@ -7,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+
 
 }
